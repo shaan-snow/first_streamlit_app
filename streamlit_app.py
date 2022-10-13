d6132d48e7b036eeccbf825b2c74cc6e1f81b704
shaan-snow <115705676+shaan-snow@users.noreply.github.com>
2022-10-13 19:04:43 +0530

    Update streamlit_app.py
    Added python code

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,7 @@ streamlit.header('Breakfast Favourties')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥙 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free-Range Egg')
-streamlit.header('Build your own fruit smoothies 🍹🍌🍊🍉🥭🍎🍇🥤')
+streamlit.header('🍹🍌🍊🍉 Build your own fruit smoothies 🥭🍎🍇🥤')
+import pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
