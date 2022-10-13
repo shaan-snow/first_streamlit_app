864a1f3a645f9cbf64a37951a95ced8358668038
shaan-snow <115705676+shaan-snow@users.noreply.github.com>
2022-10-13 19:14:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,7 @@ streamlit.header('🍹🍌🍊🍉 Build your own fruit smoothies 🥭🍎🍇�
 import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+streamlit.multiselect("Pick Fruits You Like :",list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list)
+
+                    
